d36d38f8c9aacede4933a98599a0b24c64a2eb90
VicCTS <victordm.cts@gmail.com>
2022-09-30 13:50:26 +0200

    TPS con freelook camera

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
 {
-    CharacterController controller;
+    private CharacterController controller;
+    public Transform cam;
 
     public float speed = 5;
     public float jumpHeight = 1;
@@ -14,29 +15,80 @@ public class ThirdPersonController : MonoBehaviour
     public Transform groundSensor;
     public float sensorRadius = 0.1f;
     public LayerMask ground;
-    Vector3 playerVelocity;
+    private Vector3 playerVelocity;
 
+    private float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
 
+    public Cinemachine.AxisState xAxis;
+    public Cinemachine.AxisState yAXis;
+    
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movement();
+        //MovementTPS();
+        MovementTPS2();
+
+        Jump();
+    }
+
+    void Movement()
+    {
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
         if(move != Vector3.zero)
         {
-            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
 
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
-
-            controller.Move(move * speed * Time.deltaTime);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+    }
 
+    //Movimiento TPS con Freelook camera
+    void MovementTPS()
+    {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if(move != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    //Movimiento TPS con virtaul camera
+    void MovementTPS2()
+    {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if(move != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    void Jump()
+    {
         isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
         if(isGrounded && playerVelocity.y < 0)
         {

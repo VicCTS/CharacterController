026098baf3a55a8ef90182a34ccc0f5f88126a3a
VicCTS <95314178+VicCTS@users.noreply.github.com>
2022-10-27 21:37:04 +0200

    Personaje añadido y codigo comentado

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -30,21 +30,31 @@ public class FirstPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //variables para almacenar el movimiento del raton en cada eje
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
+        //Le vamos restando el valor del movimiento del raton en el eje Y
         xRotation -= mouseY;
+        //Limitamos la rotacion de la camara entre -90 y 90 
         xRotation = Mathf.Clamp(xRotation, -90, 90);
 
+        //Rotamos la camara en el eje X (mirar arriba y abajo)
         fpsCamera.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
+        //Rotamos el personaje en el eje Y para que pueda girar sobre si mismo
         transform.Rotate(Vector3.up * mouseX);
 
+        //Almacenamos los Inputs de movimiento
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        //Creamos un vector donde multiplicamos el transform rigth por el input de movimiento lateral
+        //y el transform forward por el movimiento hacia delante para que cuando movamos el personaje
+        //se mueva en la direccion correcta
         Vector3 move = transform.right * x + transform.forward * z;
 
+        //Funcion del character controller a la que le pasamos el Vector que hemos creado y lo multiplicamos por la velocidad para movernos
         controller.Move(move.normalized * speed * Time.deltaTime);
 
         Jump();
